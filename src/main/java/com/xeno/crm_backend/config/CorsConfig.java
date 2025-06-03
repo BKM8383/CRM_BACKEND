ba9663556d29425311f78bc897953c0469e9a184
Bhupendra kumawat <122453339+BKM8383@users.noreply.github.com>
2025-06-03 23:06:52 +0530

    Update CorsConfig.java

--- src/main/java/com/xeno/crm_backend/config/CorsConfig.java
+++ src/main/java/com/xeno/crm_backend/config/CorsConfig.java
@@ -10,8 +10,7 @@ public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-            .allowedOrigins("*")
-            .allowedOrigins("http://localhost:3000")
+            .allowedOrigins("https://xenoprojectcrm.netlify.app")
             .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
             .allowedHeaders("*")
             .allowCredentials(true)
